661dd1879998f82ba1730db3bd6b32172decfe10
Lozke <17802931+Lozke@users.noreply.github.com>
2021-07-02 02:14:14 -0500

    Add permissions

--- src/main/java/land/face/mounts/commands/MountsCommand.java
+++ src/main/java/land/face/mounts/commands/MountsCommand.java
@@ -2,10 +2,7 @@ package land.face.mounts.commands;
 
 import com.tealcube.minecraft.bukkit.TextUtils;
 import com.tealcube.minecraft.bukkit.shade.acf.BaseCommand;
-import com.tealcube.minecraft.bukkit.shade.acf.annotation.CommandAlias;
-import com.tealcube.minecraft.bukkit.shade.acf.annotation.CommandCompletion;
-import com.tealcube.minecraft.bukkit.shade.acf.annotation.Default;
-import com.tealcube.minecraft.bukkit.shade.acf.annotation.Subcommand;
+import com.tealcube.minecraft.bukkit.shade.acf.annotation.*;
 import land.face.mounts.data.Mount;
 import land.face.mounts.managers.MountManager;
 import org.bukkit.entity.Player;
@@ -35,19 +32,22 @@ public class MountsCommand extends BaseCommand {
         manager.showGUI(player);
     }
 
+    @CommandPermission("EpicMounts.save")
     @Subcommand("save")
     public void onSave() {
         manager.saveMounts();
     }
 
+    @CommandPermission("EpicMounts.load")
     @Subcommand("load")
     public void onLoad() {
         manager.loadMounts();
     }
 
+    @CommandPermission("EpicMounts.debug")
     @CommandCompletion("@mount-ids")
-    @Subcommand("test")
-    public void onTest(Player player, String mountID) {
+    @Subcommand("debug")
+    public void onDebug(Player player, String mountID) {
         Mount m = manager.getMount(mountID);
         if (m == null) {
             player.sendMessage("mount is null ya dummy");
