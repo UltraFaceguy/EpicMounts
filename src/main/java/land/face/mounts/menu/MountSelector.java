c278225768206fa6988751567583c4a90bea711a
Lozke <17802931+Lozke@users.noreply.github.com>
2021-07-03 02:23:28 -0500

    Colorize name and lore

--- src/main/java/land/face/mounts/menu/MountSelector.java
+++ src/main/java/land/face/mounts/menu/MountSelector.java
@@ -1,11 +1,17 @@
 package land.face.mounts.menu;
 
+import io.pixeloutlaw.minecraft.spigot.garbage.ListExtensionsKt;
+import io.pixeloutlaw.minecraft.spigot.garbage.StringExtensionsKt;
+import io.pixeloutlaw.minecraft.spigot.hilt.ItemStackExtensionsKt;
 import land.face.mounts.EpicMountsPlugin;
 import land.face.mounts.data.Mount;
 import ninja.amp.ampmenus.events.ItemClickEvent;
 import ninja.amp.ampmenus.items.MenuItem;
 import org.bukkit.Material;
+import org.bukkit.entity.Player;
 import org.bukkit.inventory.ItemStack;
+
+import java.util.Arrays;
 
 public class MountSelector extends MenuItem {
 
@@ -14,6 +20,14 @@ public class MountSelector extends MenuItem {
     public MountSelector(Mount mount) {
         super(mount.getName(), new ItemStack(Material.SADDLE), mount.getLore());
         this.mount = mount;
+    }
+
+    @Override
+    public ItemStack getFinalIcon(Player player) {
+        ItemStack stack = new ItemStack(Material.SADDLE);
+        ItemStackExtensionsKt.setDisplayName(stack, StringExtensionsKt.chatColorize(mount.getName()));
+        ItemStackExtensionsKt.setLore(stack, ListExtensionsKt.chatColorize(Arrays.asList(mount.getLore())));
+        return stack;
     }
 
     @Override
