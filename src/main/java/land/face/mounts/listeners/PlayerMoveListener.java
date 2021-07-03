7167c5bdf77ff00236d1421c0bffd0e0bff1e631
UltraFaceguy <kattreinkyle@gmail.com>
2021-07-03 10:49:03 -0400

    Finishing SPEED

--- src/main/java/land/face/mounts/listeners/PlayerMoveListener.java
+++ src/main/java/land/face/mounts/listeners/PlayerMoveListener.java
@@ -5,43 +5,42 @@ import land.face.mounts.managers.MountManager;
 import org.bukkit.Location;
 import org.bukkit.Material;
 import org.bukkit.entity.Entity;
+import org.bukkit.entity.LivingEntity;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
 import org.bukkit.event.player.PlayerMoveEvent;
 
 import java.util.ArrayList;
+import org.bukkit.potion.PotionEffect;
+import org.bukkit.potion.PotionEffectType;
 
 public class PlayerMoveListener implements Listener {
 
-    private MountManager manager;
-    private ArrayList<Material> pathBlocks;
-    private double pathSpeed;
+  private final MountManager manager;
+  private final ArrayList<Material> pathBlocks;
 
-    public PlayerMoveListener(EpicMountsPlugin plugin) {
-        this.manager = plugin.getMountManager();
-        this.pathBlocks = new ArrayList<>();
-        this.pathSpeed = plugin.getSettings().getDouble("config.Paths.Speed");
-        for (String string : plugin.getSettings().getStringList("config.Paths.Blocks")) {
-            try {
-                pathBlocks.add(Material.getMaterial(string.toUpperCase()));
-            } catch (IllegalArgumentException e) {
-                System.out.println("[Mounts] Illegal material type for path block: " + string);
-            }
-        }
+  public PlayerMoveListener(EpicMountsPlugin plugin) {
+    this.manager = plugin.getMountManager();
+    this.pathBlocks = new ArrayList<>();
+    for (String string : plugin.getSettings().getStringList("config.Paths.Blocks")) {
+      try {
+        pathBlocks.add(Material.getMaterial(string.toUpperCase()));
+      } catch (IllegalArgumentException e) {
+        System.out.println("[Mounts] Illegal material type for path block: " + string);
+      }
     }
+  }
 
-    @EventHandler
-    public void onPlayerMove(PlayerMoveEvent event) {
-        Entity vehicle = event.getPlayer().getVehicle();
-        if (vehicle == null || !manager.isMount(vehicle)) return;
-        Location location = vehicle.getLocation().add(0, -1, 0);
-
-        //TODO: UPDATE MOUNT SPEED
-        if (pathBlocks.contains(location.getBlock().getType())) {
-            //entity.getAttribute(Attribute.GENERIC_MOVEMENT_SPEED).setBaseValue(pathSpeed * (manager.getMount(player).getSpeed() / 100));
-        }
-        else {
-            //entity.getAttribute(Attribute.GENERIC_MOVEMENT_SPEED).setBaseValue(0.2 * (manager.getMount(player).getSpeed() / 100));
-        }
+  @EventHandler
+  public void onPlayerMove(PlayerMoveEvent event) {
+    Entity vehicle = event.getPlayer().getVehicle();
+    if (!(vehicle instanceof LivingEntity) || !manager.isMount(vehicle)) {
+      return;
     }
+    Location location = vehicle.getLocation().add(0, -1, 0);
+    if (pathBlocks.contains(location.getBlock().getType())) {
+      ((LivingEntity) vehicle)
+          .addPotionEffect(new PotionEffect(PotionEffectType.SPEED, 5, 0, true, false));
+    }
+  }
 }
